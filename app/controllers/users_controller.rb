570aeb804b33d4b4acce77cd83a86376f7ec094f
Feifan Zhou <feifan@layer.com>
2016-10-20 16:04:37 -0700

    Add endpoint to list users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,5 @@
+class UsersController < ApplicationController
+  def index
+    render json: { users: User.all }
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ActiveRecord::Base
   has_secure_password
   after_initialize :set_display_name
 
+  def as_json(options = {})
+    super(options).except("password_digest")
+  end
+
   private def set_display_name
     self.display_name ||= if first_name && last_name
       "#{first_name} #{last_name}"

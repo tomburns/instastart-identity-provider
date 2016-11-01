a6c721ad8e87733a32a05b8950e3f866e14ffdc2
Feifan Zhou <feifan@layer.com>
2016-11-01 12:37:09 -0700

    CRUD UI for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,45 @@
 class UsersController < ApplicationController
   def index
-    render json: { users: User.all }
+    @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: { users: @users } }
+    end
+  end
+
+  def show
+    find_user
+    respond_to do |format|
+      format.html
+      format.json { render json: @user }
+    end
+  end
+
+  def create
+    user = User.create(user_params)
+    p user.errors
+    redirect_to user
+  end
+
+  def edit
+    find_user
+  end
+
+  def update
+    find_user
+    if @user.update_attributes(user_params)
+      render 'show'
+    else
+      render text: 'Error updating user fields'
+    end
+  end
+
+  private
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :display_name, :first_name, :last_name, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/authenticate', to: 'authentication#identity_token'
   get '/deployed', to: 'application#deployed'
-  get '/users', to: 'users#index'
+
+  resources :users
+
   root to: 'application#home'
 end

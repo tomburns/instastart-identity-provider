e6bec716a4aa86addd9013aca6e80bf74a403b80
Feifan Zhou <feifan@me.com>
2016-11-09 06:41:23 -0800

    Add liberal CORS headers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  before_filter :set_cors_preflight_headers
+  after_filter :set_cors_access_control_headers
 
   def deployed
     render text: 'ok'
@@ -10,5 +12,22 @@ class ApplicationController < ActionController::Base
     @provider_id_set = !ENV['LAYER_PROVIDER_ID'].blank?
     @private_key_set = !ENV['LAYER_PRIVATE_KEY'].blank?
     # Renders app/views/application/home.html.erb by default
+  end
+
+  def set_cors_preflight_headers
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = '*'
+      render text: ''
+    end
+  end
+
+  def set_cors_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = '*'
   end
 end

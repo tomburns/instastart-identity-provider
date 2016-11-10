47316045056e9ccdb7acd8c4ec8bd43c07cf891c
Blake Watters <blake@layer.com>
2016-11-10 14:32:20 -0800

    Drop controller code for CORS in favor of rack/cors Gem

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 
 gem 'json-jwt', '~> 1.6.5'
+gem 'rack-cors', require: 'rack/cors'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-  before_filter :set_cors_preflight_headers
-  after_filter :set_cors_access_control_headers
 
   def deployed
     render plain: 'ok'
@@ -14,20 +12,7 @@ class ApplicationController < ActionController::Base
     # Renders app/views/application/home.html.erb by default
   end
 
-  def set_cors_preflight_headers
-    if request.method == :options
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-      headers['Access-Control-Request-Method'] = '*'
-      headers['Access-Control-Allow-Headers'] = '*'
-      render plain: ''
-    end
   end
 
-  def set_cors_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = '*'
   end
 end

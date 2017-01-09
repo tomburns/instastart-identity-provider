1d33dbd0088913591ccf85d4ffd2a8aaac3048e5
Feifan Zhou <feifan@layer.com>
2017-01-09 13:16:44 -0800

    Include identity token response under 'layer_identity_token' key
    Maintain compatibility with existing sample client code

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,7 @@ class AuthenticationController < ApplicationController
     auth_request = AuthenticationRequest.new(params[:email], params[:password])
     if auth_request.valid?
       identity_token = identity_token_generator.generate_token(auth_request.user, params[:nonce])
-      render json: { identity_token: identity_token.to_s }, status: :ok
+      render json: { identity_token: identity_token.to_s, layer_identity_token: identity_token.to_s }, status: :ok
     else
       render json: { error: 'Authentication failed: verify your credentials and try again.' }, status: :unauthorized
     end
